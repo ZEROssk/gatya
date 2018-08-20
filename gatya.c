a1de72199d657f08bbb2ab9c6db12ea65337af26
Yuichi Sato <y_sato@nnn.ed.jp>
2018-08-20 14:10:34 +0900

    リファクタリング例

--- gatya.c
+++ gatya.c
@@ -2,6 +2,346 @@
 #include <stdlib.h>
 #include <time.h>
 
+typedef enum {
+	eRANK_SUPAR_RARE_BEGIN = 0,
+	eRANK_SUPAR_RARE_NAME0 = eRANK_SUPAR_RARE_BEGIN,
+	eRANK_SUPAR_RARE_NAME1,
+	eRANK_SUPAR_RARE_NAME2,
+	eRANK_SUPAR_RARE_NAME3,
+	eRANK_SUPAR_RARE_END,
+
+	eRANK_SUPAR_RARE_MAX = eRANK_SUPAR_RARE_END,
+} E_RANK_SUPAR_RARE;
+
+
+const char* strRankSuparRareNames [eRANK_SUPAR_RARE_MAX] = {
+	"アルトリア・ペンドラゴン",
+	"モードレッド",
+	"アルテラ",
+};
+
+typedef enum {
+	eRANC_RARE_BEGIN = 0,
+	eRANC_RARE_SUPER_RARE = eRANC_RARE_BEGIN,
+	eRANC_RARE_RARE,
+	eRANC_RARE_END
+
+	eRANC_RARE_MAX = eRANC_RARE_END,
+} E_RANC_TYPE;
+
+void getSuperRareCard ()
+{
+	printf("┏━━━━━━━━┓\n");
+	printf("┃ 星５鯖 ┃ ");
+	//25
+	nameselectgo = rand() % 25;     //星５鯖の個別選択
+
+	printf( " %s\n", strRankSuparRareNames[nameselectgo]);
+				
+	/* switch (nameselectgo) { */
+	/* case eRANK_SUPAR_RARE_NAME0: */
+	/*     printf(" アルトリア・ペンドラゴン\n"); */
+	/* 	break; */
+	/* case eRANK_SUPAR_RARE_NAME1: */
+	/*     printf(" モードレッド\n"); */
+	/* 	break; */
+	/* case eRANK_SUPAR_RARE_NAME2: */
+	/*     printf(" アルテラ\n"); */
+	/* 	break; */
+	/* case 3: */
+	/*     printf(" オリオン\n"); */
+	/* 	break; */
+	/* case 4: */
+	/*     printf(" ニコラ・テスラ\n"); */
+	/* 	break; */
+	/* case 5: */
+	/*     printf(" アルジュナ\n"); */
+	/* 	break; */
+	/* case 6: */
+	/*     printf(" カルナ\n"); */
+	/* 	break; */
+	/* case 7: */
+	/*     printf(" エルキドゥ\n"); */
+	/* 	break; */
+	/* case 8: */
+	/*     printf(" アルトリア・ペンドラゴン[ランサー]\n"); */
+	/* 	break; */
+	/* case 9: */
+	/*     printf(" フランシス・ドレイク\n"); */
+	/* 	break; */
+	/* case 10: */
+	/*     printf(" オジマンディアス\n"); */
+	/* 	break; */
+	/* case 11: */
+	/*     printf(" 女王メイヴ\n"); */
+	/* 	break; */
+	/* case 12: */
+	/*     printf(" ケツァル・コアトル\n"); */
+	/* 	break; */
+	/* case 13: */
+	/*     printf(" アキレウス\n"); */
+	/* 	break; */
+	/* case 14: */
+	/*     printf("玉藻の前\n"); */
+	/* 	break; */
+	/* case 15: */
+	/*     printf(" 諸葛孔明[エルメロイII世]\n"); */
+	/* 	break; */
+	/* case 16: */
+	/*     printf(" 玄装三蔵\n"); */
+	/* 	break; */
+	/* } */
+
+	/* if (nameselectgo == 17) */
+	/* { */
+	/*     printf(" 不夜城のキャスター\n"); */
+	/* } */
+	/* if (nameselectgo == 18) */
+	/* { */
+	/*     printf(" アナスタシア\n"); */
+	/* } */
+	/* if (nameselectgo == 19) */
+	/* { */
+	/*     printf(" ジャック・ザ・リッパー\n"); */
+	/* } */
+	/* if (nameselectgo == 20) */
+	/* { */
+	/*     printf(" 刑部姫\n"); */
+	/* } */
+	/* if (nameselectgo == 21) */
+	/* { */
+	/*     printf(" ヴラド三世\n"); */
+	/* } */
+	/* if (nameselectgo == 22) */
+	/* { */
+	/*     printf(" ナイチンゲール\n"); */
+	/* } */
+	/* if (nameselectgo == 23) */
+	/* { */
+	/*     printf(" クー・フーリン[オルタ]\n"); */
+	/* } */
+	/* if (nameselectgo == 24) */
+	/* { */
+	/*     printf(" ジャンヌ・ダルク\n"); */
+	/* } */
+	printf("┗━━━━━━━━┛\n");
+	kakutei = kakutei + 1;
+	gosaba = gosaba + 1;
+}
+
+void getRareCard ()
+{
+	printf("  星４鯖　　");
+	//48
+	nameselectyon = rand() % 48;        //星４鯖の個別選択
+	if (nameselectyon == 0)
+		{
+			printf(" アルトリア・ペンドラゴン[オルタ]\n");
+		}
+	if (nameselectyon == 1)
+		{
+			printf(" ネロ・クラウディウス\n");
+		}
+	if (nameselectyon == 2)
+		{
+			printf(" ガウェイン\n");
+		}
+	if (nameselectyon == 3)
+		{
+			printf(" ジークフリート\n");
+		}
+	if (nameselectyon == 4)
+		{
+			printf(" ラーマ\n");
+		}
+	if (nameselectyon == 5)
+		{
+			printf(" ランスロット\n");
+		}
+	if (nameselectyon == 6)
+		{
+			printf(" シュヴァリエ・デオン\n");
+		}
+	if (nameselectyon == 7)
+		{
+			printf(" 鈴鹿御前\n");
+		}
+	if (nameselectyon == 8)
+		{
+			printf(" エミヤ\n");
+		}
+	if (nameselectyon == 9)
+		{
+			printf(" アタランテ\n");
+		}
+	if (nameselectyon == 10)
+		{
+			printf(" トリスタン\n");
+		}
+	if (nameselectyon == 11)
+		{
+			printf(" アーチャー・インフェルノ\n");
+		}
+	if (nameselectyon == 12)
+		{
+			printf(" ケイローン\n");
+		}
+	if (nameselectyon == 13)
+		{
+			printf(" ヴラド三世[EXTRA]\n");
+		}
+	if (nameselectyon == 14)
+		{
+			printf(" エリザベート・バートリー\n");
+		}
+	if (nameselectyon == 15)
+		{
+			printf(" フィン・マックール\n");
+		}
+	if (nameselectyon == 16)
+		{
+			printf(" 李書文\n");
+		}
+	if (nameselectyon == 17)
+		{
+			printf(" アルトリア・ペンドラゴン[オルタ][ランサー]\n");
+		}
+	if (nameselectyon == 18)
+		{
+			printf(" メドゥーサ\n");
+		}
+	if (nameselectyon == 19)
+		{
+			printf(" パールヴァティー\n");
+		}
+	if (nameselectyon == 20)
+		{
+			printf(" 哪吒\n");
+		}
+	if (nameselectyon == 21)
+		{
+			printf(" アストルフォ\n");
+		}
+	if (nameselectyon == 22)
+		{
+			printf(" アン・ボニー＆メアリー・リード\n");
+		}
+	if (nameselectyon == 23)
+		{
+			printf(" マリー・アントワネット\n");
+		}
+	if (nameselectyon == 24)
+		{
+			printf(" マルタ\n");
+		}
+	if (nameselectyon == 25)
+		{
+			printf(" ナーサリー・ライム\n");
+		}
+	if (nameselectyon == 26)
+		{
+			printf(" ニトクリス\n");
+		}
+	if (nameselectyon == 27)
+		{
+			printf(" メディア[リリィ]\n");
+		}
+	if (nameselectyon == 28)
+		{
+			printf(" ギルガメッシュ\n");
+		}
+	if (nameselectyon == 29)
+		{
+			printf(" エレナ・ブラヴァツキー\n");
+		}
+	if (nameselectyon == 30)
+		{
+			printf(" トーマス・エジソン\n");
+		}
+	if (nameselectyon == 31)
+		{
+			printf(" オケアノスのキャスター\n");
+		}
+	if (nameselectyon == 32)
+		{
+			printf(" ステンノ\n");
+		}
+	if (nameselectyon == 33)
+		{
+			printf(" エミヤ[アサシン]\n");
+		}
+	if (nameselectyon == 34)
+		{
+			printf(" カーミラ\n");
+		}
+	if (nameselectyon == 35)
+		{
+			printf(" 新宿のアサシン\n");
+		}
+	if (nameselectyon == 36)
+		{
+			printf(" 不夜城のアサシン\n");
+		}
+	if (nameselectyon == 37)
+		{
+			printf(" アサシン・パライソ\n");
+		}
+	if (nameselectyon == 38)
+		{
+			printf(" ヘラクレス\n");
+		}
+	if (nameselectyon == 39)
+		{
+			printf(" ランスロット\n");
+		}
+	if (nameselectyon == 40)
+		{
+			printf(" フランケンシュタイン\n");
+		}
+	if (nameselectyon == 41)
+		{
+			printf(" ベオウルフ\n");
+		}
+	if (nameselectyon == 42)
+		{
+			printf(" タマモキャット\n");
+		}
+	if (nameselectyon == 43)
+		{
+			printf(" 茨木童子\n");
+		}
+	if (nameselectyon == 44)
+		{
+			printf(" エルドラドのバーサーカー\n");
+		}
+	if (nameselectyon == 45)
+		{
+			printf(" アタランテ[オルタ]\n");
+		}
+	if (nameselectyon == 46)
+		{
+			printf(" ゴルゴーン\n");
+		}
+	if (nameselectyon == 47)
+		{
+			printf(" 新宿のアヴェンジャー\n");
+		}
+	kakutei = kakutei + 1;
+	yonsaba = yonsaba + 1;
+}
+
+void printTotalResult () {
+	/*  ガチャ結果を種類ごとに枚数表示  */
+    printf("\n┏ ガチャ結果━━━┓\n");
+    printf("┃ 星５鯖　　%d枚┃\n", gosaba);
+    printf("┃ 星４鯖　　%d枚┃\n", yonsaba);
+    printf("┃ 星３鯖　　%d枚┃\n", sansaba);
+    printf("┃ 星５礼装　%d枚┃\n", goreisou);
+    printf("┃ 星４礼装　%d枚┃\n", yonreisou);
+    printf("┃ 星３礼装　%d枚┃\n", sanreisou);
+    printf("┗━━━━━━━━━━━━━━┛");
+}
+
 int main(void)
 {
     clock_t start, end;
@@ -41,311 +381,9 @@ int main(void)
             kakuritu = rand() % 100;
             if (kakuritu <= haisyutu[0])    //星５鯖の排出処理
             {
-                printf("┏━━━━━━━━┓\n");
-                printf("┃ 星５鯖 ┃ ");
-                //25
-                nameselectgo = rand() % 25;     //星５鯖の個別選択
-                if (nameselectgo == 0)
-                {
-                    printf(" アルトリア・ペンドラゴン\n");
-                }
-                if (nameselectgo == 1)
-                {
-                    printf(" モードレッド\n");
-                }
-                if (nameselectgo == 2)
-                {
-                    printf(" アルテラ\n");
-                }
-                if (nameselectgo == 3)
-                {
-                    printf(" オリオン\n");
-                }
-                if (nameselectgo == 4)
-                {
-                    printf(" ニコラ・テスラ\n");
-                }
-                if (nameselectgo == 5)
-                {
-                    printf(" アルジュナ\n");
-                }
-                if (nameselectgo == 6)
-                {
-                    printf(" カルナ\n");
-                }
-                if (nameselectgo == 7)
-                {
-                    printf(" エルキドゥ\n");
-                }
-                if (nameselectgo == 8)
-                {
-                    printf(" アルトリア・ペンドラゴン[ランサー]\n");
-                }
-                if (nameselectgo == 9)
-                {
-                    printf(" フランシス・ドレイク\n");
-                }
-                if (nameselectgo == 10)
-                {
-                    printf(" オジマンディアス\n");
-                }
-                if (nameselectgo == 11)
-                {
-                    printf(" 女王メイヴ\n");
-                }
-                if (nameselectgo == 12)
-                {
-                    printf(" ケツァル・コアトル\n");
-                }
-                if (nameselectgo == 13)
-                {
-                    printf(" アキレウス\n");
-                }
-                if (nameselectgo == 14)
-                {
-                    printf("玉藻の前\n");
-                }
-                if (nameselectgo == 15)
-                {
-                    printf(" 諸葛孔明[エルメロイII世]\n");
-                }
-                if (nameselectgo == 16)
-                {
-                    printf(" 玄装三蔵\n");
-                }
-                if (nameselectgo == 17)
-                {
-                    printf(" 不夜城のキャスター\n");
-                }
-                if (nameselectgo == 18)
-                {
-                    printf(" アナスタシア\n");
-                }
-                if (nameselectgo == 19)
-                {
-                    printf(" ジャック・ザ・リッパー\n");
-                }
-                if (nameselectgo == 20)
-                {
-                    printf(" 刑部姫\n");
-                }
-                if (nameselectgo == 21)
-                {
-                    printf(" ヴラド三世\n");
-                }
-                if (nameselectgo == 22)
-                {
-                    printf(" ナイチンゲール\n");
-                }
-                if (nameselectgo == 23)
-                {
-                    printf(" クー・フーリン[オルタ]\n");
-                }
-                if (nameselectgo == 24)
-                {
-                    printf(" ジャンヌ・ダルク\n");
-                }
-                printf("┗━━━━━━━━┛\n");
-                kakutei = kakutei + 1;
-                gosaba = gosaba + 1;
+				getSuperRareCard ();
             } else if (kakuritu <= haisyutu[1]) {   //星４鯖の排出処理
-                printf("  星４鯖　　");
-                //48
-                nameselectyon = rand() % 48;        //星４鯖の個別選択
-                if (nameselectyon == 0)
-                {
-                    printf(" アルトリア・ペンドラゴン[オルタ]\n");
-                }
-                if (nameselectyon == 1)
-                {
-                    printf(" ネロ・クラウディウス\n");
-                }
-                if (nameselectyon == 2)
-                {
-                    printf(" ガウェイン\n");
-                }
-                if (nameselectyon == 3)
-                {
-                    printf(" ジークフリート\n");
-                }
-                if (nameselectyon == 4)
-                {
-                    printf(" ラーマ\n");
-                }
-                if (nameselectyon == 5)
-                {
-                    printf(" ランスロット\n");
-                }
-                if (nameselectyon == 6)
-                {
-                    printf(" シュヴァリエ・デオン\n");
-                }
-                if (nameselectyon == 7)
-                {
-                    printf(" 鈴鹿御前\n");
-                }
-                if (nameselectyon == 8)
-                {
-                    printf(" エミヤ\n");
-                }
-                if (nameselectyon == 9)
-                {
-                    printf(" アタランテ\n");
-                }
-                if (nameselectyon == 10)
-                {
-                    printf(" トリスタン\n");
-                }
-                if (nameselectyon == 11)
-                {
-                    printf(" アーチャー・インフェルノ\n");
-                }
-                if (nameselectyon == 12)
-                {
-                    printf(" ケイローン\n");
-                }
-                if (nameselectyon == 13)
-                {
-                    printf(" ヴラド三世[EXTRA]\n");
-                }
-                if (nameselectyon == 14)
-                {
-                    printf(" エリザベート・バートリー\n");
-                }
-                if (nameselectyon == 15)
-                {
-                    printf(" フィン・マックール\n");
-                }
-                if (nameselectyon == 16)
-                {
-                    printf(" 李書文\n");
-                }
-                if (nameselectyon == 17)
-                {
-                    printf(" アルトリア・ペンドラゴン[オルタ][ランサー]\n");
-                }
-                if (nameselectyon == 18)
-                {
-                    printf(" メドゥーサ\n");
-                }
-                if (nameselectyon == 19)
-                {
-                    printf(" パールヴァティー\n");
-                }
-                if (nameselectyon == 20)
-                {
-                    printf(" 哪吒\n");
-                }
-                if (nameselectyon == 21)
-                {
-                    printf(" アストルフォ\n");
-                }
-                if (nameselectyon == 22)
-                {
-                    printf(" アン・ボニー＆メアリー・リード\n");
-                }
-                if (nameselectyon == 23)
-                {
-                    printf(" マリー・アントワネット\n");
-                }
-                if (nameselectyon == 24)
-                {
-                    printf(" マルタ\n");
-                }
-                if (nameselectyon == 25)
-                {
-                    printf(" ナーサリー・ライム\n");
-                }
-                if (nameselectyon == 26)
-                {
-                    printf(" ニトクリス\n");
-                }
-                if (nameselectyon == 27)
-                {
-                    printf(" メディア[リリィ]\n");
-                }
-                if (nameselectyon == 28)
-                {
-                    printf(" ギルガメッシュ\n");
-                }
-                if (nameselectyon == 29)
-                {
-                    printf(" エレナ・ブラヴァツキー\n");
-                }
-                if (nameselectyon == 30)
-                {
-                    printf(" トーマス・エジソン\n");
-                }
-                if (nameselectyon == 31)
-                {
-                    printf(" オケアノスのキャスター\n");
-                }
-                if (nameselectyon == 32)
-                {
-                    printf(" ステンノ\n");
-                }
-                if (nameselectyon == 33)
-                {
-                    printf(" エミヤ[アサシン]\n");
-                }
-                if (nameselectyon == 34)
-                {
-                    printf(" カーミラ\n");
-                }
-                if (nameselectyon == 35)
-                {
-                    printf(" 新宿のアサシン\n");
-                }
-                if (nameselectyon == 36)
-                {
-                    printf(" 不夜城のアサシン\n");
-                }
-                if (nameselectyon == 37)
-                {
-                    printf(" アサシン・パライソ\n");
-                }
-                if (nameselectyon == 38)
-                {
-                    printf(" ヘラクレス\n");
-                }
-                if (nameselectyon == 39)
-                {
-                    printf(" ランスロット\n");
-                }
-                if (nameselectyon == 40)
-                {
-                    printf(" フランケンシュタイン\n");
-                }
-                if (nameselectyon == 41)
-                {
-                    printf(" ベオウルフ\n");
-                }
-                if (nameselectyon == 42)
-                {
-                    printf(" タマモキャット\n");
-                }
-                if (nameselectyon == 43)
-                {
-                    printf(" 茨木童子\n");
-                }
-                if (nameselectyon == 44)
-                {
-                    printf(" エルドラドのバーサーカー\n");
-                }
-                if (nameselectyon == 45)
-                {
-                    printf(" アタランテ[オルタ]\n");
-                }
-                if (nameselectyon == 46)
-                {
-                    printf(" ゴルゴーン\n");
-                }
-                if (nameselectyon == 47)
-                {
-                    printf(" 新宿のアヴェンジャー\n");
-                }
-                kakutei = kakutei + 1;
-                yonsaba = yonsaba + 1;
+				getRareCard ();
             } else if (kakuritu <= haisyutu[2]) {   //星５礼装の排出処理
                 printf("┏━━━━━━━━━━┓\n");
                 printf("┃ 星５礼装 ┃");
@@ -787,17 +825,9 @@ int main(void)
         }
     }
 
-/*  ガチャ結果を種類ごとに枚数表示  */
-    printf("\n┏ ガチャ結果━━━┓\n");
-    printf("┃ 星５鯖　　%d枚┃\n", gosaba);
-    printf("┃ 星４鯖　　%d枚┃\n", yonsaba);
-    printf("┃ 星３鯖　　%d枚┃\n", sansaba);
-    printf("┃ 星５礼装　%d枚┃\n", goreisou);
-    printf("┃ 星４礼装　%d枚┃\n", yonreisou);
-    printf("┃ 星３礼装　%d枚┃\n", sanreisou);
-    printf("┗━━━━━━━━━━━━━━┛");
+	printTotalResult ();
 
-    end = clock();
+	end = clock();
     printf("\n処理時間%lu[ms]\n", end - start);
 
     return 0;
